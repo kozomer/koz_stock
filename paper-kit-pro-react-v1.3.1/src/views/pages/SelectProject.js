ac12aff5890847575358b643c5db5fbcf5c4d20c
emik61 <kozemirhan98@gmail.com>
2023-09-22 16:51:01 +0300

    1

--- paper-kit-pro-react-v1.3.1/src/views/pages/SelectProject.js
+++ paper-kit-pro-react-v1.3.1/src/views/pages/SelectProject.js
@@ -5,6 +5,8 @@ import project1Image from '../../assets/img/kozoran.png';
 import project2Image from '../../assets/img/koz_logo.png';
 import '../../assets/css/Table.css';
 import localforage from 'localforage';
+
+import ReactBSAlert from "react-bootstrap-sweetalert";
 const projects = [
   { project_id: 1, title:'', imageUrl: project1Image },
   { project_id: 2, title: '', imageUrl:  project2Image },
@@ -12,13 +14,34 @@ const projects = [
   // ...
 ];
 
+
 function SelectionPage() {
     const [selectedProject, setSelectedProject] = useState(null);
     const history = useHistory();
+
+    const [alert, setAlert] = useState(null);
+
+const errorUpload = (e) => {
+  console.log("asdsad")
+  setAlert(
+    <ReactBSAlert
+      danger
+      style={{ display: "block", marginTop: "-100px" }}
+      title="Error"
+      onConfirm={() => hideAlert()}
+      onCancel={() => hideAlert()}
+      confirmBtnBsStyle="info"
+      btnSize=""
+    >
+      {e}
+    </ReactBSAlert>
+  );
+};
   
     async function handleSelectProject(project)  {
       setSelectedProject(project);
       const selectedData={project_id:project.project_id}
+      console.log(selectedData)
       
       const access_token = await localforage.getItem('access_token'); 
        fetch(`${process.env.REACT_APP_PUBLIC_URL}/set_current_project/`,{
@@ -29,26 +52,36 @@ function SelectionPage() {
           'Authorization': 'Bearer '+ String(access_token)
         }
       })
-        .then((response) => {
-      if (!response.ok) {
-        return response.json().then(data => {
-            console.log(data)
-            
-        });
-      }
-      else{
-        return response.json().then(data => {
-          
-            history.push('../admin/dashboard');
-          console.log("Error")
-        });
-      }
+      .then((response) => {
+        if (!response.ok) {
+            return response.json().then(data => {
+                // Check if there's an error key in the data and call errorUpload
+                if (data.error) {
+                    errorUpload(data.error);
+                }
+            });
+        } else {
+            return response.json().then(data => {
+              console.log(data)
+              errorUpload(data);
+                history.push('../admin/dashboard');
+                
+            });
+        }
     })
+    .catch(err => {
+        // This will catch any network errors or JSON parsing errors
+        errorUpload(err);
+    });
+
        // replace '/next-page' with the path to the next page
     }
   
     return (
+      <div className='content'>
+           {alert}
       <Container className="containers">
+        
         <Row>
           {projects.map((project) => (
             <Col xs="12" sm="6" md={projects.length > 2 ? "4" : "6"} key={project.project_id} className="column"> 
@@ -78,6 +111,7 @@ function SelectionPage() {
           ))}
         </Row>
       </Container>
+      </div>
     );
   }
 
