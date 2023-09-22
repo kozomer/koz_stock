c1db6f3693f72c4e1d54135ef50956cb56228cdc
kozomer <kozomer@gmail.com>
2023-09-22 12:26:18 +0300

    debug

--- koz_stock/koz_stock/settings.py
+++ koz_stock/koz_stock/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 #! True for developement environment
 #! False for production environment
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["192.168.1.192",
                  "127.0.0.1",
